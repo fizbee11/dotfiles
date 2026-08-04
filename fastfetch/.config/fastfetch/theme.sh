2225d781cca8649f35274edfb7ef6d9126999dd9
fizbee11 <fizbee11@gmail.com>
2026-08-04 13:04:10 +0530

    changed to nord/catpucchin theme

--- fastfetch/.config/fastfetch/theme.sh
+++ fastfetch/.config/fastfetch/theme.sh
@@ -3,7 +3,7 @@ time3="7/29/2024 9:00PM"
 let current=$(date +%s)
 timestamp3=$(date -d "$time3" +%s)
 theme_progression=$((current - timestamp3))
-theme_done=$((theme_progression/ 86400))
 
 
-echo $theme_done Days on Nord
+echo $theme_progression Days on Nord
+
